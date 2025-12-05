5eef9227d64b56d51caa7409d942afb5821b0be8
Jubeth S B <sjubeth3@gmail.com>
2025-12-05 13:15:30 +0530

    Add Prometheus and Grafana monitoring stack

--- terraform/main.tf
+++ terraform/main.tf
@@ -13,9 +13,13 @@ provider "docker" {
   host = "npipe:////./pipe/docker_engine"
 }
 
+# --- NETWORK ---
+
 resource "docker_network" "devops_net" {
   name = "devops_network"
 }
+
+# --- CI/CD SERVER (JENKINS) ---
 
 resource "docker_image" "jenkins" {
   name         = "jenkins/jenkins:lts"
@@ -40,8 +44,58 @@ resource "docker_container" "jenkins_server" {
     name = docker_network.devops_net.name
   }
   
+  # Allow Jenkins to control Docker (Docker-in-Docker)
   volumes {
     host_path      = "/var/run/docker.sock"
     container_path = "/var/run/docker.sock"
+  }
+}
+
+# --- MONITORING STACK ---
+
+# 1. Prometheus (The Data Collector)
+resource "docker_image" "prometheus" {
+  name         = "prom/prometheus:latest"
+  keep_locally = false
+}
+
+resource "docker_container" "prometheus" {
+  name  = "prometheus-server"
+  image = docker_image.prometheus.name
+  
+  ports {
+    internal = 9090
+    external = 9090
+  }
+  
+  networks_advanced {
+    name = docker_network.devops_net.name
+  }
+
+  # Mount the config file dynamically
+  # This looks for the file in: local-devops-project/monitoring/prometheus.yml
+  volumes {
+    host_path      = abspath("${path.cwd}/../monitoring/prometheus.yml")
+    container_path = "/etc/prometheus/prometheus.yml"
+  }
+}
+
+# 2. Grafana (The Dashboard)
+resource "docker_image" "grafana" {
+  name         = "grafana/grafana:latest"
+  keep_locally = false
+}
+
+resource "docker_container" "grafana" {
+  name  = "grafana-server"
+  image = docker_image.grafana.name
+  
+  ports {
+    internal = 3000
+    external = 3000
+  }
+  
+  networks_advanced {
+    name = docker_network.devops_net.name
   }
 }
